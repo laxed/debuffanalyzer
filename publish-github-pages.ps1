5d0c0bfea94ba379a56bfe599a4d1e6b3454d6d4
Andreas Kromann <kromann.andreas@gmail.com>
2020-06-13 23:01:55 +0200

    Trying to publish again now in docs

--- publish-github-pages.ps1
+++ publish-github-pages.ps1
@@ -1,13 +1,11 @@
 $pathToSolution = "./src/"
-$projectName = "AutoML.CodeGenerator.Blazor"
-$repoName = "AutoML.CodeGenerator"
 
 Write-Output "----==== Publish $pathToSolution"
 dotnet publish $pathToSolution -c Release -o ./dist/
 Write-Output ""
 
-Write-Output "----==== Copy from ./dist/$projectName/dist"
-Copy-Item -Path "./dist/wwwroot/*" -Destination "./" -Recurse -Force
+Write-Output "----==== Copy from ./dist/wwwroot to ./docs"
+Copy-Item -Path "./dist/wwwroot/*" -Destination "./docs" -Recurse -Force
 Write-Output ""
 
 Write-Output "----==== Delete dist folder"
